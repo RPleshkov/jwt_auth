e78629cbec7e550e3872bbf3d73670bfa5662f34
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-07 00:35:16 +0300

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -4,12 +4,21 @@ import uvicorn
 from api import router
 from fastapi import FastAPI
 from messaging import router as nats_router
+from fastapi.middleware.cors import CORSMiddleware
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 app = FastAPI(title="AUTH_PRACTICE")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 app.include_router(nats_router)
